c19f24ce0fcdf8848831cc4dcd580c5fa17a32c1
Henrique Lima <hnamix@gmail.com>
2021-02-25 15:26:03 -0400

    exercicios do bloco 4.2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Bloco4_2_8CriarArray.js
@@ -0,0 +1,8 @@
+let array=[];
+
+for(let i = 1; i<=25; i++){
+  array.push(i);
+  }
+
+for(let i=0; i<array.length; i++)  
+console.log(array[i]);
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Bloco4_2_9Divisaopor2.js
@@ -0,0 +1,10 @@
+let array=[];
+
+for(let i = 0; i<=25; i++){
+  array.push(i);
+  }
+
+for(let i = 1; i<array.length; i++){
+  
+  console.log(array[i]/2);
+}
