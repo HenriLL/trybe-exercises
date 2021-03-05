6792b147b2b36c6364bd9a57f1147e83a97af315
Henrique Lima <hnamix@gmail.com>
2021-03-05 12:22:52 -0400

    Exercicios Parte II DOM

--- exercises/javascript-dom-eventos-e-web-storage_2/exerc.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/exerc.js
@@ -11,3 +11,34 @@ document.querySelector('#elementoOndeVoceEsta').nextSibling;
 
 document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 
+
+
+
+//Exercício 2
+
+let elemento = document.querySelector('#pai');
+let novoElemento;
+novoElemento = document.createElement('div');
+novoElemento.className = 'irmao';
+elemento.appendChild(novoElemento);
+
+
+let selecionaElemento = document.querySelector('#elementoOndeVoceEsta');
+let novoFilho = document.createElement('div');
+novoFilho.className ='filhoOndeVoceEsta';
+selecionaElemento.appendChild(novoFilho);
+
+let selecionaPrimeiro = document.querySelector('#primeiroFilho');
+let filhoPrimeiro = document.createElement('div');
+filhoPrimeiro.className = 'filhoDoprimeiroFilho';
+selecionaElemento.appendChild(filhoPrimeiro);
+
+document.querySelector('.filhoDoprimeiroFilho').parentNode.nextElementSibling
+
+
+/*
+***Crie um irmão para elementoOndeVoceEsta .
+***Crie um filho para elementoOndeVoceEsta .
+***Crie um filho para primeiroFilhoDoFilho .
+***A partir desse filho criado, acesse terceiroFilho .*/
+
