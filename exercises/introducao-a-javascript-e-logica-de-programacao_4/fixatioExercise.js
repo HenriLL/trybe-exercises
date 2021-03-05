4b06687be0fbf1db06874a709866f30f439c5bd5
Henrique Lima <hnamix@gmail.com>
2021-03-05 10:03:48 -0400

    exercicio 5.1 DOM

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/fixatioExercise.js
@@ -0,0 +1,27 @@
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player ={
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3
+  }
+}
+
+// "A jogadora Marta Silva tem 34 anos de idade"
+
+console.log("A jogadora "+ player.name + " "+player.lastName+" tem "+ player.age + " anos de idade");
+
+// let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+player['bestInTheWorld']=[2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(player.bestInTheWorld);
+
+
+
---
+++ exercises/javascript-dom-eventos-e-web-storage_2/exerc.js
@@ -0,0 +1,13 @@
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "red";
+
+document.querySelector('#primeiroFilhoDoFilho').innerHTML = "Alterando o texto!";
+
+
+document.querySelector('#pai').firstChild;
+
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
+
+document.querySelector('#elementoOndeVoceEsta').nextSibling;
+
+document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+
