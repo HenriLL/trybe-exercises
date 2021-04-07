d38ac81deee5ba6afca3c6e592e3d2e9bcde8b88
Henrique Lima <hnamix@gmail.com>
2021-04-07 16:18:01 -0400

    resoluçao de exercicios bloco 6.1

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/expt2ex3.js
@@ -0,0 +1,7 @@
+const bot = document.getElementById('botao');
+let clickCount = 0;
+let textToDisplay = document.getElementById("text")
+bot.addEventListener('click', () => textToDisplay.innerHTML = clickCount += 1);
+alert(count)
+//Solução obtida na resolução de exercícios
+
---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/expt2ex4.js
@@ -0,0 +1,30 @@
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"]
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    )
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`)
+
+    result = `
+    ${result}
+
+    #goTrybe
+    `
+
+    return result
+}
+
+console.log(buildSkillsPhrase("Lucas"))
+
+//solução da resolução de exercícios
