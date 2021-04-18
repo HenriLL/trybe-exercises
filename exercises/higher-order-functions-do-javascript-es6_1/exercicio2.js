98f0d81c593203539905e6eec694276ab63d416a
Henrique Lima <hnamix@gmail.com>
2021-04-18 09:54:42 -0400

    Exercicio 02

--- exercises/higher-order-functions-do-javascript-es6_1/exercicio2.js
+++ exercises/higher-order-functions-do-javascript-es6_1/exercicio2.js
@@ -17,3 +17,15 @@
 // // console.log(check(4, sorteio));
 // // console.log(check(5, sorteio));
 
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+// hof(num, check)-> gera aleatório(1-5) / retorna resultado
+const geraNumero = (num, checkNumero) => {
+  const numero =(Math.floor(Math.random() * 5))+1;
+  console.log(numero);
+  if (checkNumero(num, numero)) return 'Parabéns  você ganhou';
+  return 'Tente novamente';
+};
+
+const checkNumero = (num, numAleatorio) => num === numAleatorio;
+console.log(geraNumero(1, checkNumero));
