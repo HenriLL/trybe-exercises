05aacc37ccd03a96d0eae29c230f45bf1e99b639
Henrique Lima <hnamix@gmail.com>
2021-05-21 15:25:49 -0400

    exercicios do bloco 9.1

--- exercises/javascript-assincrono-e-promises_1/aula.js
+++ exercises/javascript-assincrono-e-promises_1/aula.js
@@ -1,6 +1,6 @@
 // console.log('1 - Receber roda');
 // console.log('2 - Encaixar parafusos');
-// console.log('3 - Fixar roda no carro');
+// // console.log('3 - Fixar roda no carro');
 
 // console.log('1 - Comprar parafusos');
 // console.log('2 - Adicionar ao estoque');
@@ -27,10 +27,7 @@
 // pushNumber(numbers, 2);
 // pushNumber(numbers, 3);
 
-// console.log(numbers)
-
 // assert.deepStrictEqual(numbers, [1, 2, 3]);
-
 
 // const assert = require('assert');
 
@@ -41,8 +38,8 @@
 // setTimeout(() => pushNumber(numbers, 1), 3000);
 // pushNumber(numbers, 2);
 // pushNumber(numbers, 3);
-// console.log(numbers);
-// // assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+
+// assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
 
 // const assert = require('assert');
 
@@ -57,39 +54,91 @@
 // pushNumber(numbers, 2);
 // pushNumber(numbers, 3);
 
-// setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+// // setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
 
-const myExpenses = [
-  {
-    gym: 99,
-  },
-  {
-    ifood: 200,
-  },
-  {
-    phone: 60,
-  },
-  {
-    internet: 100,
-  },
-];
+// const myExpenses = [
+//   {
+//     gym: 99,
+//   },
+//   {
+//     ifood: 200,
+//   },
+//   {
+//     phone: 60,
+//   },
+//   {
+//     internet: 100,
+//   },
+// ];
 
-const myIncome = 1000;
+// const myIncome = 1000;
 
-const monthlyBudget = (myIncome, myExpenses, callback) => {
+// const monthlyBudget = (myIncome, myExpenses, callback) => {
 
-  const totalExpenses = callback(myExpenses);
-  const totalAfterExpenses = myIncome - totalExpenses;
+//   const totalExpenses = callback(myExpenses);
+//   const totalAfterExpenses = myIncome - totalExpenses;
 
-  console.log(`Balanço do mês:
-    Recebido: R$${myIncome},00
-    Gasto: R$${totalExpenses},00
-    Saldo: R$${totalAfterExpenses},00 `);
+//   console.log(`Balanço do mês:
+//     Recebido: R$${myIncome},00
+//     Gasto: R$${totalExpenses},00
+//     Saldo: R$${totalAfterExpenses},00 `);
+// };
+
+// const handleExpenses = myExpenses => {
+//   const eachValue = myExpenses.map((item) => Object.values(item)[0]);
+//   const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
+//   return totalExpense;
+// };
+
+// monthlyBudget(myIncome, myExpenses, handleExpenses);
+
+// // No código abaixo você tem a função getUser , que retorna uma pessoa qualquer. Complete a função getUser de forma que ela receba uma outra função como parâmetro para que possa realizar as operações abaixo sobre a pessoa retornada.
+
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+// const getUser = () => {
+//   setTimeout(() => {
+//     const user = {
+//       firstName: "Ivan",
+//       lastName: "Ivanovich",
+//       nationality: "Russian",
+//     };
+//     console.log(user);
+//   }, delay());
+// };
+
+// getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+// getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+    }
+  }, delay());
 };
-const handleExpenses = myExpenses => {
-  const eachValue = myExpenses.map((item) => Object.values(item)[0]);
-  const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
-  return totalExpense;
-};
 
-monthlyBudget(myIncome, myExpenses, handleExpenses);
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
