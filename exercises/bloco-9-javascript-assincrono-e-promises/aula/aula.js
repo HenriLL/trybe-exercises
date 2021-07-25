3113fe2a38dd09367450d5b853f76e0d941568a6
Henrique Lima <hnamix@gmail.com>
2021-07-25 10:32:07 -0400

    parte 1 aula

---
+++ exercises/bloco-9-javascript-assincrono-e-promises/aula/aula.js
@@ -0,0 +1,46 @@
+// const tryb = {
+//   "trybers": [
+//     {
+//       "name": "Lauro Cesar",
+//       "github": "lauroandrade",
+//       "status": "#vqv"
+//     }
+//   ]
+// }
+// // console.log(tryb);
+// wget 'https://pokeapi.co/api/v2/pokemon/ditto' -O - -q
+
+// const promise = new Promise((resolve, reject) => {
+
+// });
+
+// const promise = new Promise((resolve, reject) => {
+//   const number = Math.floor(Math.random() * 11);
+
+//   if (number <= 5) {
+//     return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+//   }
+//   resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+// });
+// const promise = new Promise((resolve, reject) => {
+//   const number = Math.floor(Math.random() * 11);
+
+//   if (number <= 5) {
+//     return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+//   }
+//   resolve(number);
+// })
+// .then(number => `Que sucesso =) nosso número foi ${number}`)
+// .then(msg => console.log(msg));
+
+// const promise = new Promise((resolve, reject) => {
+//   const number = Math.floor(Math.random()* 11);
+
+//   if (number <= 5) {
+//     return reject(number);
+//   }
+//   resolve(number);
+// })
+// .then(number => `Que sucesso =) nosso número foi ${number}`)
+// .then(msg => console.log(msg))
+// .catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
